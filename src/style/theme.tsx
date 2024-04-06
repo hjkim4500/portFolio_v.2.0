dfc05fef7f3cc43b542c7de75672d22d5b6fa77a
hjkim4500 <83331110+hjkim4500@users.noreply.github.com>
2024-04-06 19:52:24 +0900

    Portfolio make..

---
+++ src/style/theme.tsx
@@ -0,0 +1,10 @@
+const theme = {
+    color: {
+        primary: "#03a9f4", // 주 색상
+        secondary: "#795548", // 부 색상
+        white: "#FFFFFF",
+        gray: "#CCCCCC",
+        default: "#999999", // 기본 문자 색상
+        error: "#FF0000", // 오류 색상
+    },
+};
